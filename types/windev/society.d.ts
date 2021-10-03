ce601b30a89bb39490412d3520486addd0a712c9
Thomas GENTILHOMME <gentilhomme.thomas@gmail.com>
2021-10-03 13:54:36 +0200

    fix: remove enum for DiaryType & fix Diary interface

--- types/windev/society.d.ts
+++ types/windev/society.d.ts
@@ -75,30 +75,34 @@ export interface Exercice {
   closed_by: null | number;
 }
 
-export enum DiaryType {
-  ACH = 1,
-  VTE = 2,
-  BQ = 3,
-  CAISSE = 4,
-  OD = 5,
-  OD_SIMUL = 7,
-  A_EXT = 8,
-  EXT = 9,
-  AN = 10,
-  OD_PAIE = 11,
-  OD_LET = 12,
-  NDF = 13,
-  OD_DECL_FISCALE = 14,
-  OD_EXC = 15
+export interface DiaryType {
+  ACH: 1,
+  VTE: 2,
+  BQ: 3,
+  CAISSE: 4,
+  OD: 5,
+  OD_SIMUL: 7,
+  A_EXT: 8,
+  EXT: 9,
+  AN: 10,
+  OD_PAIE: 11,
+  OD_LET: 12,
+  NDF: 13,
+  OD_DECL_FISCALE: 14,
+  OD_EXC: 15
 }
 
 export interface Diary<T = DiaryType> {
   code: string;
   name: string;
-  account: null | string;
+  account: null | {
+    id: number;
+    label: string;
+    number: string;
+  };
   blocked: boolean;
   diary_id: number;
-  diary_type_id: null | T[keyof T];
+  diary_type_id: T[keyof T];
   diary_type_code: keyof T;
   diary_type_name: string;
 }
