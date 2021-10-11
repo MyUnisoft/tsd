97bd80360f426c04a32ae31914d3b87100d5de55
SofianD <doual.sofian@hotmail.com>
2021-10-11 14:55:33 +0200

    feat: update country interface

--- types/windev/country.d.ts
+++ types/windev/country.d.ts
@@ -1,5 +1,9 @@
 interface Country {
   id: number;
+
+  /** Code ISO du pays ([ISO 3166](https://fr.wikipedia.org/wiki/ISO_3166)). */
   iso: string;
+  
+  /** Libellé du pays en français et en majuscule. */
   label: string;
 }
